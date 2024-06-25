bbf73d97c3ab1065d8f1e58a0c4c3faa5f8b05de
Mayara <mayaradepaula1997>
2024-06-25 18:52:40 -0300

    Cors Config

---
+++ src/main/java/br/com/turmajava/turmajava/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package br.com.turmajava.turmajava.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://controle-sala-de-turmas.netlify.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
